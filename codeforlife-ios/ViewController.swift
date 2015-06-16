ae261bd14fa7e0f81f61cbd3a06a72d6ba707fbb
Joey Chan <joey.chan@MB14470.local>
2015-06-16 14:36:58 +0100

    Initial commit with empty project

---
+++ codeforlife-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  codeforlife-ios
+//
+//  Created by Joey Chan on 16/06/2015.
+//  Copyright (c) 2015 Joey Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
