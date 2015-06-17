d0ea70c7dafff6b9de1e8fc2f64f3d305deebed2
Clifford Bailey <clifford.bailey@ocado.com>
2015-06-17 18:57:22 +0100

    Adding a static webview that loads level 1

--- codeforlife-ios/ViewController.swift
+++ codeforlife-ios/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
 
+    @IBOutlet weak var gameWebView: UIWebView!
+    
+    @IBAction func back(sender: AnyObject){
+        
+    }
+    
+    @IBAction func forward(sender: AnyObject){
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = NSURL(string: "https://www.codeforlife.education/rapidrouter/1/")
+        let requestObj = NSURLRequest(URL: url!)
+        gameWebView.loadRequest(requestObj)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
