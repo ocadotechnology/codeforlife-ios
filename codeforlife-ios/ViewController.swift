5fd84a21c2f76a34ee00132665f12460696cec65
Clifford Bailey <clifford.bailey@ocado.com>
2015-06-17 21:05:44 +0100

    Really hacky proof of concept that loads a few native buttons and strips the menu.

--- codeforlife-ios/ViewController.swift
+++ codeforlife-ios/ViewController.swift
@@ -20,6 +20,22 @@ class ViewController: UIViewController, UIWebViewDelegate {
         
     }
     
+    @IBAction func didPressClear(sender: AnyObject){
+        self.clearScreen()
+    }
+    
+    @IBAction func didPressPlay(sender: AnyObject){
+        self.play()
+    }
+    
+    @IBAction func didPressStop(sender: AnyObject){
+        self.stop()
+    }
+    
+    @IBAction func didPressStepOver(sender: AnyObject){
+        self.stepOver()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -32,6 +48,39 @@ class ViewController: UIViewController, UIWebViewDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        //remove non-native clear button
+        self.removeTabMenu()
+    }
+    
+    func clearScreen(){
+        gameWebView.stringByEvaluatingJavaScriptFromString("ocargo.blocklyControl.reset();");
+        gameWebView.stringByEvaluatingJavaScriptFromString("ocargo.game.reset();");
+//        gameWebView.stringByEvaluatingJavaScriptFromString("$('#clear_radio').trigger('click');")
+    }
+    
+    func play(){
+        gameWebView.stringByEvaluatingJavaScriptFromString("$('#play_radio').trigger('click');")
+    }
+    
+    func stop(){
+        gameWebView.stringByEvaluatingJavaScriptFromString("$('#stop_radio').trigger('click');")
+    }
+    
+    func stepOver(){
+        gameWebView.stringByEvaluatingJavaScriptFromString("$('#step_radio').trigger('click');")
+    }
+    
+    func removeTabMenu(){
+        gameWebView.stringByEvaluatingJavaScriptFromString("var divsToHide = document.getElementsByClassName('tab');" +
+                                                           "for(var i = 0; i < divsToHide.length; i++){" +
+                                                           "divsToHide[i].style.visibility='hidden';}")
+        
+        gameWebView.stringByEvaluatingJavaScriptFromString("var divsToHide = document.getElementsByClassId('tabs');" +
+            "for(var i = 0; i < divsToHide.length; i++){" +
+            "divsToHide[i].style.visibility='hidden';}")
+    }
+    
 }
 
