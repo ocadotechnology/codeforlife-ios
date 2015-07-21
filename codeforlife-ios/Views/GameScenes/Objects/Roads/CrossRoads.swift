774da5b680d95d0ec2b044f1cfee13c3103be8ee
Joey Chan <joey.chan@MB19208.local>
2015-07-21 11:17:02 +0100

    Fixed position bug and change ui of message views

---
+++ codeforlife-ios/Views/GameScenes/Objects/Roads/CrossRoads.swift
@@ -0,0 +1,9 @@
+//
+//  CrossRoads.swift
+//  codeforlife-ios
+//
+//  Created by Joey Chan on 21/07/2015.
+//  Copyright (c) 2015 Joey Chan. All rights reserved.
+//
+
+import Foundation
---
+++ codeforlife-ios/Views/GameScenes/Objects/Roads/Straight.swift
@@ -0,0 +1,9 @@
+//
+//  Straight.swift
+//  codeforlife-ios
+//
+//  Created by Joey Chan on 21/07/2015.
+//  Copyright (c) 2015 Joey Chan. All rights reserved.
+//
+
+import Foundation
